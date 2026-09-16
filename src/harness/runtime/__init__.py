2366a5ac0066c4548da58a25da3a3cedfad340a2
rafa210587 <rafael210587@gmail.com>
2026-09-15 23:58:56 -0300

    chore: export runtime factories

--- src/harness/runtime/__init__.py
+++ src/harness/runtime/__init__.py
@@ -1,3 +1,11 @@
 from harness.runtime.agent_loop import AgentLoop, AgentRunResult, AgentStatus, ApprovalHandler
+from harness.runtime.factory import build_agent_loop, build_tool_registry
 
-__all__ = ["AgentLoop", "AgentRunResult", "AgentStatus", "ApprovalHandler"]
+__all__ = [
+    "AgentLoop",
+    "AgentRunResult",
+    "AgentStatus",
+    "ApprovalHandler",
+    "build_agent_loop",
+    "build_tool_registry",
+]
