303498184818719b86388b9ec201b251d159873b
rafa210587 <rafael210587@gmail.com>
2026-09-15 23:57:24 -0300

    chore: initialize runtime hooks package

---
+++ src/harness/hooks/__init__.py
@@ -0,0 +1,12 @@
+from harness.hooks.base import BeforeToolEvent, HookAction, HookDecision, RuntimeHook
+from harness.hooks.dispatcher import HookDispatcher
+from harness.hooks.permissions import PermissionHook
+
+__all__ = [
+    "BeforeToolEvent",
+    "HookAction",
+    "HookDecision",
+    "HookDispatcher",
+    "PermissionHook",
+    "RuntimeHook",
+]
